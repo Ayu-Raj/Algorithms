8831293f6785e6c9f6b7036eaa38e8edcdf60a92
Saket prag <43814993+sakigo9@users.noreply.github.com>
2020-10-07 19:02:03 +0530

    Update halfSort.c
    User Input

--- halfSort.c
+++ halfSort.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 void main() {
 	int i,j,mid,k;
-	int arr[6] = {5,2,6,3,9,1};
+	int n;
+	scanf("%d",&n);
+	
+	int arr[n];
+	for(i=0;i<n;i++) scanf("%d",arr[i]);
 	mid = 3;
-	for (i=0;i<6-1;i++){
-		for(j=0;j<6-i-1-mid;j++){
+	for (i=0;i<n-1;i++){
+		for(j=0;j<n-i-1-mid;j++){
 			if(arr[j]>arr[j+1])
 			swap(&arr[j], &arr[j+1]);
 		}
-		for(j=mid;j<6-i-1;j++){
+		for(j=mid;j<n-i-1;j++){
 			if(arr[j]<arr[j+1])
 			swap(&arr[j], &arr[j+1]);
 		}
 	}
-	for(k=0;k<6;k++){
+	for(k=0;k<n;k++){
 		printf("%d\n", arr[k]);
 	}
 
